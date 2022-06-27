5637a6f6703f947f4dfbee85110150acbcfc29cc
LAPTOP-F1K0K0MV\ヒューマン次郎三郎 <sendai.program@gmail.com>
2022-06-27 09:50:08 +0900

    6/27シカのペンキ

--- PGBATTLE/main.cpp
+++ PGBATTLE/main.cpp
@@ -1,13 +1,44 @@
 #include <iostream>
+#include <set>
 
 using namespace std;
 
 int main(void)
 {
-	int a, b, h;
-	cin >> a >> b >> h;
+	set<int> colors;
 
-	cout << (a + b) * h / 2 << endl;
+	for (int i = 0; i < 3; i++) {
+		int color_no;
+		cin >> color_no;
+		colors.insert(color_no);
+	}
+
+	cout << colors.size() << endl;
+	// ƒtƒ‰ƒO‚ð—§‚Ä‚é
+	//bool colors[100]{ false };
+	//for (int i = 0; i < 3; i++) {
+	//	int color_no;
+	//	cin >> color_no;
+	//	colors[color_no] = true;
+	//}
+
+	//int count = 0;
+	//for (int i = 0; i < 100; i++) {
+	//	if (colors[i])
+	//		count++;
+	//}
+
+	//cout << count << endl;
+
+	// if•¶‚Å”»’è‚·‚é
+	//if (a != b && a != c && b != c)
+	//	cout << 3 << endl;
+	//if ((a == b && a != c)
+	// || (a == c && a != b)
+	// || (b == c && b != a))
+	//	cout << 2 << endl;
+	//if (a == b && a == c)
+	//	cout << 1 << endl;
 
 	return 0;
 }
