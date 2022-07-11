438ebc11206f94a55d2426a8b760f552c05722e2
LAPTOP-F1K0K0MV\ヒューマン次郎三郎 <sendai.program@gmail.com>
2022-07-11 09:29:36 +0900

    7/11 HonestOrDishonest

--- PGBATTLE/main.cpp
+++ PGBATTLE/main.cpp
@@ -4,14 +4,22 @@ using namespace std;
 
 int main(void)
 {
-	int n;
-	cin >> n;
+	char a, b;
+	cin >> a >> b;
 
-	int price = 800 * n;
-	int back  = (n / 15) * 200;
-	cout << price - back << endl;
+	if (a == b)
+		cout << "H" << endl;
+	else
+		cout << 'D' << endl;
 
-	// cout << 800 * n - (n / 15) * 200 << endl;
+	//if (a == 'H' && b == 'H')
+	//	cout << 'H' << endl;
+	//else if (a == 'H' && b == 'D')
+	//	cout << 'D' << endl;
+	//else if (a == 'D' && b == 'H')
+	//	cout << 'D' << endl;
+	//else if (a == 'D' && b == 'D')
+	//	cout << 'H' << endl;
 
 	return 0;
 }
