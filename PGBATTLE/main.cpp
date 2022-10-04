8436db6675907a901002552ad865c74f7a086901
LAPTOP-F1K0K0MV\ヒューマン次郎三郎 <sendai.program@gmail.com>
2022-10-04 10:51:16 +0900

    10/4 PG BATTLE せんべい1

--- PGBATTLE/main.cpp
+++ PGBATTLE/main.cpp
@@ -3,15 +3,44 @@
 
 using namespace std;
 
+int factorial(int n);
+int combination(int n, int r);
+
 int main(void)
 {
-	int x, y, k;
-	cin >> x >> y >> k;
+	int p;
+	cin >> p;
 
-	//double answer = ((double)x * y - k) / x;
-	double answer = y - (double)k / x;
-	cout << std::setprecision(15);
-	cout << answer << endl;
+	double win  = p / 100.0;	// Ÿ‚ÂŠm—¦
+	double lose = 1.0 - win;	// •‰‚¯‚éŠm—¦
+
+	// 4-0
+	double win4_0 = win * win * win * win;
+	// 4-1
+	double win4_1 = combination(4, 3) * win * win * win * win * lose;
+	// 4-2
+	double win4_2 = combination(5, 3) * win * win * win * win * lose * lose;
+	// 4-3
+	double win4_3 = combination(6, 3) * win * win * win * win * lose * lose * lose;
+
+	cout << fixed << setprecision(12);
+	cout << (win4_0 + win4_1 + win4_2 + win4_3) * 100.0 << endl;
 
 	return 0;
+}
+
+int combination(int n, int r)
+{
+	int nCr = factorial(n) / (factorial(r) * factorial(n - r));
+	return nCr;
+}
+
+int factorial(int n)
+{
+	int answer = 1;
+	for (int i = 2; i <= n; i++) {
+		answer *= i;
+	}
+
+	return answer;
 }
