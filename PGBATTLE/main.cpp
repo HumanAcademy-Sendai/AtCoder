ccf299f2029831aec64c43f38aafa44faa571192
LAPTOP-F1K0K0MV\ヒューマン次郎三郎 <sendai.program@gmail.com>
2022-10-05 09:54:54 +0900

    10/5 PG BATTLE かつおぶし1

--- PGBATTLE/main.cpp
+++ PGBATTLE/main.cpp
@@ -1,46 +1,15 @@
 #include <iostream>
-#include <iomanip>
 
 using namespace std;
 
-int factorial(int n);
-int combination(int n, int r);
-
 int main(void)
 {
-	int p;
-	cin >> p;
+	int n;
+	cin >> n;
 
-	double win  = p / 100.0;	// Ÿ‚ÂŠm—¦
-	double lose = 1.0 - win;	// •‰‚¯‚éŠm—¦
-
-	// 4-0
-	double win4_0 = win * win * win * win;
-	// 4-1
-	double win4_1 = combination(4, 3) * win * win * win * win * lose;
-	// 4-2
-	double win4_2 = combination(5, 3) * win * win * win * win * lose * lose;
-	// 4-3
-	double win4_3 = combination(6, 3) * win * win * win * win * lose * lose * lose;
-
-	cout << fixed << setprecision(12);
-	cout << (win4_0 + win4_1 + win4_2 + win4_3) * 100.0 << endl;
+	double digits1 = 0.5 * log10(2.0 * 3.14159265358979323846264 * n);
+	double digits2 = n * log10(n / 2.718281828459);
+	cout << (int)(digits1 + digits2) + 1 << endl;
 
 	return 0;
-}
-
-int combination(int n, int r)
-{
-	int nCr = factorial(n) / (factorial(r) * factorial(n - r));
-	return nCr;
-}
-
-int factorial(int n)
-{
-	int answer = 1;
-	for (int i = 2; i <= n; i++) {
-		answer *= i;
-	}
-
-	return answer;
 }
