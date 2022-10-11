5c67938e7a2fef0822984c6ef991df893c277876
LAPTOP-F1K0K0MV\ヒューマン次郎三郎 <sendai.program@gmail.com>
2022-10-11 10:27:55 +0900

    10/11 PG BATTLE ましゅまろ2(2020)

--- PGBATTLE/main.cpp
+++ PGBATTLE/main.cpp
@@ -4,15 +4,30 @@ using namespace std;
 
 int main(void)
 {
-	int n, x, a, b;
-	cin >> n >> x >> a >> b;
+	int n;
+	cin >> n;
 
-	int answer = 0;// n* (n < x ? a : b);
-	if (n < x)
-		answer = n * a;
+	// Aの合計温度を求める
+	int sum_a = 0;
+	for (int i = 0; i < n; i++) {
+		int a;
+		cin >> a;
+		sum_a += a;
+	}
+
+	// Aの合計温度から、Bの温度を引く
+	for (int i = 0; i < n; i++) {
+		int b;
+		cin >> b;
+		sum_a -= b;
+	}
+
+	if (sum_a > 0)
+		cout << "A" << endl;
+	else if (sum_a < 0)
+		cout << "B" << endl;
 	else
-		answer = n * b;
-	cout << answer << endl;
+		cout << "same" << endl;
 
 	return 0;
 }
