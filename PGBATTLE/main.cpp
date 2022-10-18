0b5c35e5e304d68f36c9d3335b65283814ed582c
LAPTOP-F1K0K0MV\ヒューマン次郎三郎 <sendai.program@gmail.com>
2022-10-18 10:18:07 +0900

    10/18 PG BATTLE ましゅまろ2(2019)

--- PGBATTLE/main.cpp
+++ PGBATTLE/main.cpp
@@ -1,16 +1,27 @@
 #include <iostream>
+#include <string>
+#include <regex>
 
 using namespace std;
 
 int main(void)
 {
-    int w, k, d;
-    cin >> w >> k >> d;
+    string s;
+    cin >> s;
 
-    if (d >= std::max(k, w - k))
+    if (s == "AtCoder")
+    {
         cout << "Yes" << endl;
+    }
     else
-        cout << "No" << endl;
+    {
+        std::regex  re("AtCoder", std::regex_constants::icase);
+        std::smatch m;
+        if (std::regex_search(s, m, re))
+            cout << "Maybe" << endl;
+        else
+            cout << "No" << endl;
+    }
 
     return 0;
 }
