7d972e3a476d45e89620cd6c76a9d54f0bb86fc0
LAPTOP-F1K0K0MV\ヒューマン次郎三郎 <sendai.program@gmail.com>
2022-10-25 09:52:26 +0900

    10/25 PG BATTLE せんべい1(2022)

--- PGBATTLE/main.cpp
+++ PGBATTLE/main.cpp
@@ -1,17 +1,28 @@
 #include <iostream>
-#include <string>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
 int main(void)
 {
-    string s;
-    cin >> s;
+    int n, q;
+    cin >> n >> q;
 
-    cout << "81" << s.substr(1) << endl;
-    //for (int i = 1; i < s.length(); ++i)
-    //    cout << s[i];
-    //cout << endl;
+    vector<bool> back_flag(n + 1, 0);   // false...•\, true...— 
+    for (int i = 0; i < q; ++i) {
+        int L, R;
+        cin >> L >> R;
+        for (int j = L; j <= R; ++j)
+            back_flag[j] = !back_flag[j];
+    }
+
+    int answer = std::count_if(
+        back_flag.begin() + 1,
+        back_flag.end(),
+        [](bool flag) { return flag; }
+    );
+    cout << answer << endl;
 
     return 0;
 }
