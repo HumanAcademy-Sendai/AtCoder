1b9fe723590c9f3027b7f035a0b6e5bf6e2bcaf6
LAPTOP-F1K0K0MV\ヒューマン次郎三郎 <sendai.program@gmail.com>
2022-11-08 10:07:29 +0900

    11/8 C - Colorful Leaderboard

--- PGBATTLE/main.cpp
+++ PGBATTLE/main.cpp
@@ -4,20 +4,32 @@ using namespace std;
 
 int main(void)
 {
-    string s;
-    cin >> s;
+    enum COLORS {
+        GRAY, BROWN,  GREEN,  LIGHTBLUE,
+        BLUE, YELLOW, ORANGE, RED,
+        FREE,
+    };
 
-    string text;
-    for (int i = 0; i < s.length(); ++i) {
-        if (s[i] != 'B') {
-            text.push_back(s[i]);
-        }
-        else {
-            if(!text.empty())
-                text.pop_back();
-        }
+    int n;
+    cin >> n;
+
+    int colors[9]{};
+    for (int i = 0; i < n; i++) {
+        int a;
+        cin >> a;
+
+        const int index = a / 400;
+        if(index < FREE)
+            colors[index] = 1;
+        else
+            ++colors[FREE];
     }
-    cout << text << endl;
+
+    int answer = 0;
+    for (int i = 0; i < FREE; ++i)
+        answer += colors[i];
+
+    cout << answer << " " << answer + colors[FREE] << endl;
 
     return 0;
 }
