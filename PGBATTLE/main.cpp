1624872e7f3815c0efe3e023b9b440c43102e574
LAPTOP-F1K0K0MV\ヒューマン次郎三郎 <sendai.program@gmail.com>
2022-11-29 10:01:35 +0900

    11/29 C - To Infinity

--- PGBATTLE/main.cpp
+++ PGBATTLE/main.cpp
@@ -4,18 +4,32 @@ using namespace std;
 
 int main(void)
 {
-    int n;
-    cin >> n;
+    string s;
+    cin >> s;
 
-    int answer = 0;
-    for (int i = 1; i <= n; ++i) {
-        int a;
-        cin >> a;
-        //if (i % 2 != 0 && a % 2 != 0)
-        if ((i & a & 0x01))
-            ++answer;
+    long long k;
+    cin >> k;
+
+    if (k == 1) {
+        cout << s[0] << endl;
     }
-    cout << answer << endl;
+    else {
+        // 1‚ªk•¶ŽšˆÈã•À‚ñ‚Å‚¢‚é‚©?
+        // •À‚ñ‚Å‚¢‚½‚ç1‚ª“š‚¦
+        // •À‚ñ‚Å‚¢‚È‚©‚Á‚½‚çA1‚ÌŽŸ‚Éo‚Ä‚«‚½”Žš‚ª“š‚¦
+        int one_count = 0;
+        for (int i = 0; i < s.length(); ++i) {
+            if (s[i] == '1')
+                ++one_count;
+            else
+                break;
+        }
+
+        if (one_count >= k)
+            cout << '1' << endl;
+        else
+            cout << s[one_count] << endl;
+    }    
 
     return 0;
 }
