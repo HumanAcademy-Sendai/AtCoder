d39dd7b656431b31b4a262c271c49f236489f82a
LAPTOP-F1K0K0MV\ヒューマン次郎三郎 <sendai.program@gmail.com>
2022-12-14 09:37:24 +0900

    12/14 B - Counting Roads

--- PGBATTLE/main.cpp
+++ PGBATTLE/main.cpp
@@ -1,30 +1,22 @@
 #include <iostream>
-#include <map>
 
 using namespace std;
 
 int main(void)
 {
-    int n;
-    cin >> n;
+    int n, m;
+    cin >> n >> m;
 
-    long long k;
-    cin >> k;
-
-    map<int, long long> number;
-    for (int i = 0; i < n; ++i) {
-        long long a, b;
+    int road[50 + 1]{};
+    for (int i = 0; i < m; ++i) {
+        int a, b;
         cin >> a >> b;
-        number[a] += b;
+        ++road[a];
+        ++road[b];
     }
 
-    for (int i = 1; i < number.size(); ++i) {
-        k -= number[i];
-        if (k <= 0) {
-            cout << i << endl;
-            break;
-        }
-    }
+    for (int i = 1; i < n; ++i)
+        cout << road[i] << endl;
 
     return 0;
 }
