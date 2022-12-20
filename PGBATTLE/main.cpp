dfd90b74d443d1cefaad804e4a73204778e83fa5
LAPTOP-F1K0K0MV\ヒューマン次郎三郎 <sendai.program@gmail.com>
2022-12-20 10:12:11 +0900

    12/20 C - Multiple Clocks

--- PGBATTLE/main.cpp
+++ PGBATTLE/main.cpp
@@ -1,31 +1,21 @@
 #include <iostream>
+#include <numeric>
 
 using namespace std;
 
 int main(void)
 {
-    string s[3];
-    cin >> s[0];
-    cin >> s[1];
-    cin >> s[2];
+    int n;
+    cin >> n;
 
-    // 何枚目のカードを使っているか
-    int number[3]{};
-
-    // 誰のターンかを入れる
-    int turn = 0;   // Aのターンから
-    while (true) {
-        // 勝利条件
-        if (number[turn] >= s[turn].length()) {
-            cout << (char)('A' + turn) << endl;
-            break;
-        }
-
-        // 指名された人が場にカードを出す
-        int card = s[turn][number[turn]];
-        ++number[turn];
-        turn = card - 'a';
+    long long answer = 1;
+    for (int i = 0; i < n; ++i) {
+        long long t;
+        cin >> t;
+        answer = lcm(t, answer);
     }
+
+    cout << answer << endl;
 
     return 0;
 }
