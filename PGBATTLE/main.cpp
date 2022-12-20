bf797a7ff184d463cccf0e8c419de025969ead8c
LAPTOP-F1K0K0MV\ヒューマン次郎三郎 <sendai.program@gmail.com>
2022-12-20 10:11:15 +0900

    Revert "C - Multiple Clocks"
    This reverts commit 199fbf24c1588a569107893f42facc2d9add8317.

--- PGBATTLE/main.cpp
+++ PGBATTLE/main.cpp
@@ -1,21 +1,31 @@
 #include <iostream>
-#include <numeric>
 
 using namespace std;
 
 int main(void)
 {
-    int n;
-    cin >> n;
+    string s[3];
+    cin >> s[0];
+    cin >> s[1];
+    cin >> s[2];
 
-    long long answer = 1;
-    for (int i = 0; i < n; ++i) {
-        long long t;
-        cin >> t;
-        answer = lcm(t, answer);
+    // 何枚目のカードを使っているか
+    int number[3]{};
+
+    // 誰のターンかを入れる
+    int turn = 0;   // Aのターンから
+    while (true) {
+        // 勝利条件
+        if (number[turn] >= s[turn].length()) {
+            cout << (char)('A' + turn) << endl;
+            break;
+        }
+
+        // 指名された人が場にカードを出す
+        int card = s[turn][number[turn]];
+        ++number[turn];
+        turn = card - 'a';
     }
-
-    cout << answer << endl;
 
     return 0;
 }
