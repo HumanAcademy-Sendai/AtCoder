61ccf42cb901f85146f252b47cb92654f724b0d3
LAPTOP-F1K0K0MV\ヒューマン次郎三郎 <sendai.program@gmail.com>
2022-12-21 10:08:57 +0900

    12/21 つるかめ算

--- PGBATTLE/main.cpp
+++ PGBATTLE/main.cpp
@@ -1,21 +1,44 @@
 #include <iostream>
-#include <numeric>
 
 using namespace std;
 
 int main(void)
 {
-    int n;
-    cin >> n;
+    int n, m;
+    cin >> n >> m;
 
-    long long answer = 1;
-    for (int i = 0; i < n; ++i) {
-        long long t;
-        cin >> t;
-        answer = lcm(t, answer);
+    int a, b;
+    cin >> a >> b;
+
+    // 全部生物A?
+    if (a * n == m) {
+        cout << n << " 0" << endl;
+        return 0;
     }
 
-    cout << answer << endl;
+    // 全部生物B?
+    if (b * n == m) {
+        cout << "0 " << n << endl;
+        return 0;
+    }
+
+    // 足の差がない場合は解なし
+    if ((b - a) == 0) {
+        cout << "-1 -1" << endl;
+        return 0;
+    }
+
+    // 全部生物Aと仮定したときに、
+    // 足りない足の数を求める
+    int insufficient = m - a * n;
+    if (insufficient % (b - a) != 0) {
+        // 割り切れない場合は解なし
+        cout << "-1 -1" << endl;
+        return 0;
+    }
+
+    int answer_b = insufficient / (b - a);
+    cout << n - answer_b << " " << answer_b << endl;
 
     return 0;
 }
