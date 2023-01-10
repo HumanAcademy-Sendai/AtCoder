9637be983b93a7b16e852fe744b73a0c4ba677eb
LAPTOP-F1K0K0MV\ヒューマン次郎三郎 <sendai.program@gmail.com>
2023-01-10 09:53:23 +0900

    1/10 B - Shift only

--- PGBATTLE/main.cpp
+++ PGBATTLE/main.cpp
@@ -1,44 +1,29 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 int main(void)
 {
-    int n, m;
-    cin >> n >> m;
+    int n;
+    cin >> n;
 
-    int a, b;
-    cin >> a >> b;
+    int answer = INT_MAX;
+    vector<int> a;
+    a.assign(n, 0);
+    for (int i = 0; i < n; ++i) {
+        cin >> a[i];
 
-    // 全部生物A?
-    if (a * n == m) {
-        cout << n << " 0" << endl;
-        return 0;
+        // 2で割れる回数を調べる
+        int count = 0;
+        while (a[i] % 2 == 0) {
+            a[i] /= 2;
+            ++count;
+        }
+        answer = min(answer, count);
     }
 
-    // 全部生物B?
-    if (b * n == m) {
-        cout << "0 " << n << endl;
-        return 0;
-    }
-
-    // 足の差がない場合は解なし
-    if ((b - a) == 0) {
-        cout << "-1 -1" << endl;
-        return 0;
-    }
-
-    // 全部生物Aと仮定したときに、
-    // 足りない足の数を求める
-    int insufficient = m - a * n;
-    if (insufficient % (b - a) != 0) {
-        // 割り切れない場合は解なし
-        cout << "-1 -1" << endl;
-        return 0;
-    }
-
-    int answer_b = insufficient / (b - a);
-    cout << n - answer_b << " " << answer_b << endl;
+    cout << answer << endl;
 
     return 0;
 }
