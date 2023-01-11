a4c7d42812affe1ea07d0a9dc7da7f7049806ec3
LAPTOP-F1K0K0MV\ヒューマン次郎三郎 <sendai.program@gmail.com>
2023-01-11 09:28:42 +0900

    1/11 B - OddString

--- PGBATTLE/main.cpp
+++ PGBATTLE/main.cpp
@@ -1,29 +1,15 @@
 #include <iostream>
-#include <vector>
 
 using namespace std;
 
 int main(void)
 {
-    int n;
-    cin >> n;
+    string s;
+    cin >> s;
 
-    int answer = INT_MAX;
-    vector<int> a;
-    a.assign(n, 0);
-    for (int i = 0; i < n; ++i) {
-        cin >> a[i];
-
-        // 2‚ÅŠ„‚ê‚é‰ñ”‚ğ’²‚×‚é
-        int count = 0;
-        while (a[i] % 2 == 0) {
-            a[i] /= 2;
-            ++count;
-        }
-        answer = min(answer, count);
-    }
-
-    cout << answer << endl;
+    for (int i = 0; i < s.length(); i += 2)
+        cout << s[i];
+    cout << endl;
 
     return 0;
 }
